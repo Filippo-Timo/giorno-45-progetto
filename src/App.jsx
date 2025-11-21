2a8dde3320a402c1853f7b968acffad2934d9a96
Filippo-Timo <filtimo2002@gmail.com>
2025-11-21 12:02:42 +0100

    adds Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import MyNavbar from './components/MyNavbar'
+
 
 function App() {
 
   return (
     <>
-
+      <MyNavbar />
     </>
   )
 }
